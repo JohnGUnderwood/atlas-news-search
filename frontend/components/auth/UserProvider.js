3633631a53a91a64ae3d0f8a0db86cdcd79426f1
JohnGUnderwood <4124456+JohnGUnderwood@users.noreply.github.com>
2024-06-06 08:03:30 +0100

    Read api url env variable at runtime

--- frontend/components/auth/UserProvider.js
+++ frontend/components/auth/UserProvider.js
@@ -2,20 +2,20 @@ import { UserContext, ApiContext } from './UserContext';
 import jwt from 'jsonwebtoken';
 import axios from 'axios';
 
-export function UserProvider({ token, children }) {
-  const API_URL = process.env.API_URL || 'http://localhost:3010';
+export function UserProvider({ apiUrl, token, children }) {
+
   const api = axios.create({
-    baseURL: `${API_URL}/`,
+    baseURL: `${apiUrl}/`,
   });
 
   const decoded = token ? jwt.decode(token) : null;
   const user = decoded?.sub || null;
   const groups = decoded?.groups || [];
   if(user && groups){
-    api.defaults.headers.common['User'] = user;
-    api.defaults.headers.common['Groups'] = groups;
+      api.defaults.headers.common['User'] = user;
+      api.defaults.headers.common['Groups'] = groups;
   }
-  
+
   return (
     <UserContext.Provider value={{user,groups}}>
       <ApiContext.Provider value={api}>
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -3,23 +3,24 @@ import Sidebar from '../components/sidebar/sidebar';
 import SearchPage from './search';
 import Header from '../components/head';
 import { UserProvider } from '../components/auth/UserProvider';
-import FeedsPage
- from './feeds';
+import FeedsPage from './feeds';
+
 export async function getServerSideProps(context) {
     let token = process.env.NEXT_PUBLIC_LOCALDEVJWTOVERRIDE ? process.env.NEXT_PUBLIC_LOCALDEVJWTOVERRIDE : context.req.headers['x-kanopy-internal-authorization'];
-
     if(!token){
         token = null;
     }
+
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:3010';
+
     // Pass the token to your component as a prop
-    return { props: { token } };
+    return { props: { apiUrl, token } };
 }
 
-
-export default function Home({token}){
+export default function Home({apiUrl,token}){
     const [nav, setNav] = useState("search");
     return (
-        <UserProvider token={token}>
+        <UserProvider apiUrl={apiUrl} token={token}>
             <Header/>
             <div style={{display:"inline-flex",flexDirection:"row",justifyContent:"end"}}>
             { nav == 'search' ? <SearchPage/> : <FeedsPage/> }
